1f3d2cf2dd8bb5e486685ac30839cb23bbaa6a6e
ServianB <benjamin.servian@edu.esiee.fr>
2022-11-30 11:52:33 +0100

    Avancement création carte

--- carte.py
+++ carte.py
@@ -1,8 +1,21 @@
 import pandas as pd
+import numpy
 import folium
 
 #Préparation des données numeriques
-pop_data = pd.read_csv('tableau-de-bord-mci-commune-techno.csv', sep=';',header=2)
+pop_data = pd.read_csv('tableau-de-bord-mci-commune-techno.csv', sep=';',header=1,encoding='latin-1',dtype={
+    "Code Insee": int,
+    "Commune": "string",
+    " Nombre de locaux ": int,
+    "Fibre": float,
+    "Câble": float,
+    "DSL": float,
+    "THD Radio": float,
+    "4G Fixe": float,
+    "HD Radio": numpy.date,
+    "Satellite" : float,
+
+    })
 # La colonne code_insee contient le code INSEE des communes:
 dpt_code = pop_data['code_insee']
 
@@ -23,10 +36,10 @@ coords = (48.7190835,2.4609723)
 map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=9)
 
 folium.Choropleth(
-    geo_data='datagouv-communes.geojson',                              # geographical data
+    geo_data='idf.geojson',                              # geographical data
     name='choropleth',
     data=pop_data,                                  # numerical data
-    columns=['DEPCOM', 'PTOT'],                     # numerical data key/value pair
+    columns=['code_insee', 'nbr'],                     # numerical data key/value pair
     key_on='feature.properties.code_commune',       # geographical property used to establish correspondance with numerical data
     fill_color='YlGn',
     fill_opacity=0.7,
