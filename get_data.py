49da2f64109606a661fce09f4f0f05809a950c54
ServianB <benjamin.servian@edu.esiee.fr>
2022-12-31 11:23:01 +0100

    Amélioration de get_data.py

--- get_data.py
+++ get_data.py
@@ -1,9 +1,25 @@
 import requests
+from zipfile import ZipFile
 
+#récupération du fichier geoJson
+def recupererGeojsonFile(URL,name):
+    # On télécharge le fichier surlequel pointe l'URL
+    response = requests.get(URL)
+    # On sauve réponse dans un nouveau fichier appelé stackoverflow.ico
+    open(name, "wb").write(response.content)
+
+
+#on récupere le fichier des communes 
 URL = "https://perso.esiee.fr/~courivad/python_advanced/_downloads/8578d763bdb7d7d0d1a7aaeb2e3b4814/datagouv-communes.geojson"
+recupererGeojsonFile(URL,"datagouv-communes.geojson")
+#on récupere le fichier des départements
+URL = "https://france-geojson.gregoiredavid.fr/repo/departements.geojson"
+recupererGeojsonFile(URL,"datagouv-departements.geojson")
 
-# 2. On télécharge le fichier surlequel pointe l'URL
-response = requests.get(URL)
-
-# 3. On sauve réponse dans un nouveau fichier appelé stackoverflow.ico
-open("datagouv-communes.geojson", "wb").write(response.content)
+# Récupération du fichier csv
+file = "tableau-de-bord-mci-commune-techno.zip"
+#ouvrir le fichier zip en mode lecture
+with ZipFile(file,'r') as zip:
+        zip.printdir()
+        #extraire les fichiers
+        zip.extractall()
