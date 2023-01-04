dd4a488e00545a3d84dc1bf28d705190227e1009
victorinn <nathan.victorin@edu.esiee.fr>
2023-01-04 20:14:53 +0100

    Changement de la carte

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import carte
 # Data
 #
 
+#Récupération des noms de département pour la liste déroulante
 franceDep = gpd.read_file('datagouv-departements.geojson')
 print(franceDep["nom"].head())
 
@@ -40,6 +41,15 @@ data = { year:gapminder.query("year == @year") for year in years} # (2)
 # Main
 #
 
+# def changeGraph():
+#     import ctypes
+#     a = "Hello World!"
+#     py_obj = ctypes.cast(id(a), ctypes.py_object)
+#     id(py_obj.value) # 1868526529136
+#     py_obj.value = 'Bye Bye World!'
+#     # Here we can see that `value` now points to a new object
+#     id(py_obj.value) # 1868528280112
+
 if __name__ == '__main__':
 
     app = dash.Dash(__name__) # (3)
@@ -49,35 +59,25 @@ if __name__ == '__main__':
                         size="pop",
                         hover_name="country") # (4)
 
-    
-    app.layout = html.Div(children=[
-        html.Iframe(id='mapDepartementale', srcDoc=open('myMapDepartementale.html', 'r').read())
-    ], style={'padding': 10, 'flex': 1})
-
     app.layout = html.Div(children=[
 
-                            html.H1(children=f'Pourcentage d\'installation fibre par département en 2022',
+                            html.H1(id="titre", children=f'Pourcentage d\'installation fibre par département en 2022',
                                         style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
 
-                            dcc.Dropdown(listDep, listDep[0], id='dropdown-departement'),
+                            dcc.Dropdown(listDep, listDep[0], id='dropdown-departement', #onChange=changeGraph()),
+                            ),
 
                             dcc.Graph(
                                 id='graph1',
                                 figure=fig
                             ), # (6)
 
-                            html.Div(children=f'''
-                                The graph above shows relationship between life expectancy and
-                                GDP per capita for year {year}. Each continent data has its own
-                                colour and symbol size is proportionnal to country population.
-                                Mouse over for details.
-                            '''), # (7)
-
                             html.H1(children=f'Carte d\'installation fibre en France par département en 2022',
                                         style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
 
-                            dl.Map(children=[dl.TileLayer()],
-                                style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}, id="mapDepartementale"),            
+                            html.Div(children=[
+                                html.Iframe(id='mapDepartementale', srcDoc=open('myMapDepartementale.html', 'r').read())
+                            ], style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),          
     ]
     )
 
@@ -85,4 +85,10 @@ if __name__ == '__main__':
     # RUN APP
     #
 
-    app.run_server(debug=True) # (8)
+    app.run_server(debug=False) # (8)
+
+
+
+
+
+   
